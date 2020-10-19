9dc0d590ceceb98a85a6752c1a0a1c2ec6337606
Hui <jeffrey.x.hui@gmail.com>
2020-10-19 10:01:53 -0700

    importing preset constants used for setting up apps initial state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useInterval } from './useInterval.js';
+import {
+    CANVAS_SIZE,
+    SNAKE_START,
+    APPLE_START,
+    SCALE,
+    SPEED,
+    DIRECTIONS
+} from './constants.js';
 
 function App() {
     return (
